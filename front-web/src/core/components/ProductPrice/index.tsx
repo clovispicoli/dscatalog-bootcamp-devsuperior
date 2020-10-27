eeefa3d84e4496389eb22d278c20987aa2a26511
Clovis Picoli <clovispicoli@gmail.com>
2020-10-27 07:59:41 -0300

    Add product listing with backend

--- front-web/src/core/components/ProductPrice/index.tsx
+++ front-web/src/core/components/ProductPrice/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './styles.scss';
 
 type Props = {
-    price: string;
+    price: number;
+}
+
+const formatPrice = (price: number) => {
+    return new Intl.NumberFormat('pt-BR', { minimumFractionDigits: 2 }).format(price);
 }
 
 const ProductPrice = ({ price }: Props) => (
@@ -11,7 +15,7 @@ const ProductPrice = ({ price }: Props) => (
              R$
         </span>
             <h3 className="product-price">
-                {price}
+                {formatPrice(price)}
              </h3>
     </div>
 );
