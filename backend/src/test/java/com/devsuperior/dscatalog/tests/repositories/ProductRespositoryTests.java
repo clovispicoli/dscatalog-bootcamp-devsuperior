f0c79d78acc9d17f70d316dbdb3aad738f35b18c
Clovis Picoli <clovispicoli@gmail.com>
2021-01-31 13:28:13 -0300

    Using @BeforeEach to improve the code

--- backend/src/test/java/com/devsuperior/dscatalog/tests/repositories/ProductRespositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/tests/repositories/ProductRespositoryTests.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscatalog.tests.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -16,12 +17,19 @@ public class ProductRespositoryTests {
 	@Autowired
 	private ProductRepository repository;
 	
+	private long existingId;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		existingId = 1L;
+	}
+	
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
 		
-		repository.deleteById(1L);
+		repository.deleteById(existingId);
 		
-		Optional<Product> result = repository.findById(1L);
+		Optional<Product> result = repository.findById(existingId);
 		
 		Assertions.assertFalse(result.isPresent());
 		
