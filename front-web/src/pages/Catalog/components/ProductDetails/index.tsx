40abb001c9323fa058ea23f2e65ff1e9e08ba361
Clovis Picoli <clovispicoli@gmail.com>
2021-04-17 09:45:15 -0300

    Product catalog

--- front-web/src/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -3,9 +3,6 @@ import { Link, useParams } from 'react-router-dom';
 import { ReactComponent as ArrowIcon } from 'core/assets/images/arrow.svg';
 import { Product } from 'core/types/Product';
 import { makeRequest } from 'core/utils/request';
-import { stateFromHTML } from 'draft-js-import-html';
-import { Editor } from 'react-draft-wysiwyg';
-import { EditorState } from 'draft-js';
 import ProductPrice from 'core/components/ProductPrice';
 import ProductDescriptionLoader from '../Loaders/ProductDescriptionLoader';
 import ProductInfoLoader from '../Loaders/ProductInfoLoader';
@@ -19,8 +16,6 @@ const ProductDetails = () => {
     const { productId } = useParams<ParamsType>();
     const [product, setProduct] = useState<Product>();
     const [isLoading, setIsLoading] = useState(false);
-    const contentState = stateFromHTML(product?.description || '');
-    const descriptionAsEditorState = EditorState.createWithContent(contentState);
 
     useEffect(() => {
         setIsLoading(true);
@@ -36,35 +31,33 @@ const ProductDetails = () => {
                     <ArrowIcon className="icon-goback" />
                     <h1 className="text-goback">voltar</h1>
                 </Link>
-                <div className="row">
+                <div className="product-details-info">
                     <div className="col-6 pr-5">
                         {isLoading ? <ProductInfoLoader /> : (
                             <>
                                 <div className="product-details-card text-center">
                                     <img src={product?.imgUrl} alt={product?.name} className="product-details-image" />
                                 </div>
+                                <div className="product-info-fields" >
                                 <h1 className="product-details-name">
                                     {product?.name}
                                 </h1>
                                 {product?.price && <ProductPrice price={product?.price} />}
+                                </div>
                             </>
                         )}
                     </div>
-                    <div className="col-6 product-details-card">
+                    <div className="product-details-card">
                         {isLoading ? <ProductDescriptionLoader /> : (
-                            <>
+                            <> 
                                 <h1 className="product-description-title">
                                     Descrição do produto
                                 </h1>
-                                <Editor
-                                    editorClassName="product-description-text"
-                                    editorState={descriptionAsEditorState}
-                                    toolbarHidden
-                                    readOnly
-                                />
+                              <p className="product-description-text" >
+                                  {product?.description}
+                             </p>
                             </>
                         )}
-
                     </div>
                 </div>
             </div>
